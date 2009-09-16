3c670e63f6b07137db6a0a77a9b7ad652490dab2
matt <matthewharrison@gmail.com>
2009-09-16 14:39:30 -0600

    add support for deleting.  limit based on lines.  align to left.

--- keyview.py
+++ keyview.py
@@ -66,7 +66,9 @@ class GTKKeyView:
     def __init__(self, hm):
         xml = gtk.glade.XML('keyview.libglade')
         self.window = xml.get_widget('window1')
+        self.window.connect('destroy', self.quit)
         self.key_strokes = xml.get_widget('label1')
+        self.key_strokes.set_alignment(.1, 0)
         self.menu = xml.get_widget('config-menu')
         self.font_dialog = xml.get_widget('fontselectiondialog1')
         self.font = None # text of font description from selection dialog
@@ -77,7 +79,9 @@ class GTKKeyView:
         hm.KeyDown = self.hook_manager_down_event
         hm.KeyUp = self.hook_manager_up_event
         xml.signal_autoconnect(self)
-        self.max_size = 30
+        self.max_lines = 3
+        self.show_backspace = False
+        self.keys = [] #stack of keys typed
 
     def init_menu(self):
         self.font_item = gtk.MenuItem('set font...')
@@ -141,7 +145,7 @@ class GTKKeyView:
         #hm.printevent(event)
         if self.active:
             old = self.key_strokes.get_text()
-            typed = event.Key
+            e_key = event.Key
 
             # hack to deal with ctr modifiers in emacsy way
             modifiers = []
@@ -154,15 +158,65 @@ class GTKKeyView:
             if event.Key in MODIFIERS:
                 self.pressed[event.Key] = 1
             
-            typed = KEY_MAP.get(typed, typed)
-            new_text = (old + ''.join(modifiers) + typed + postfix)[-self.max_size:]
+            typed = KEY_MAP.get(e_key, e_key)
+            if e_key == 'BackSpace' and not self.show_backspace:
+                if self.keys:
+                    self.keys.pop()
+            else:
+                self.keys.append(Text(typed, postfix=postfix, prefix=''.join(modifiers)))
+
+            # limit line lengths
+            self.keys = limit_text(self.keys, self.max_lines)
+            new_text = ''.join([repr(x) for x in self.keys])
+            
             self.update_text(new_text, self.font)
 
+def limit_text(text_list, max_lines):
+    r"""
+    >>> lines = [Text('\n')]*4
+    >>> len(limit_text(lines, 2)) #only 1 newline is possible since we're limiting to two lines
+    1
+    >>> lines = [Text(x) for x in 'foo\nbar\nbaz']
+    >>> len(limit_text(lines, 2)) #from bar to end
+    7
+    
+    """
+     # limit line lengths
+    new_line_idx = [i for i,x in enumerate(text_list) if x.text == '\n']
+    if len(new_line_idx) >= max_lines:
+        new_start = new_line_idx[-max_lines] + 1 
+        text_list = text_list[new_start:]
+    return text_list
 
-if __name__ == '__main__':
+class Text(object):
+    """Simple class to hold text and pre/postfix for it
+    """
+    def __init__(self, text, prefix='', postfix=''):
+        self.text = text
+        self.prefix = prefix
+        self.postfix = postfix
+        
+    def __repr__(self):
+        return '%s%s%s' %(self.prefix, self.text, self.postfix)
+            
+def main():
     gtk.gdk.threads_init() 
     hm = get_hook_manager()
     test = GTKKeyView(hm)
     test.window.show()
     test.window.set_keep_above(True) #ensure visibility
-    gtk.main()
+    try:
+        gtk.main()
+    except KeyboardInterrupt, e:
+        # kill the hook manager thread
+        test.hm.cancel()
+
+def test():
+    import doctest
+    doctest.testmod()
+
+if __name__ == '__main__':
+    main()
+
+    
+        
