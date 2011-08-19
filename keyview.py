c6e60b63aa64e818f8af011158e74f86d7d97db8
mbarkhau <mbarkhau@gmail.com>
2011-08-19 11:20:16 +0300

    correct default height

--- keyview.py
+++ keyview.py
@@ -245,7 +245,7 @@ def main():
     hm = get_hook_manager()
     view = GTKKeyView(hm)
     w = view.window
-    w.resize(720, 170)
+    w.resize(720, 145)
     w.set_keep_above(True) #ensure visibility
     w.set_opacity(0.8)
     w.set_decorated(False) 
